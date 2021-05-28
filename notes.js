37bcb8c5adf1c8daecbaf673a17900553e51585d
Kyle <kyleferry@icloud.com>
2021-05-28 02:37:59 +0100

    added - Adding note functionality

--- notes.js
+++ notes.js
@@ -1,6 +1,45 @@
+const fs = require('fs')
+
 const getNotes = () => 'Your notes... :)'
 
 
+const addNote = (title, body) => {
+    const notes = loadNotes()
+    const duplicateNotes = notes.filter((note) => {
+        return note.title === title
+    })
 
-module.exports = getNotes
+    if(duplicateNotes.length === 0) {
+        notes.push({
+            title,
+            body
+        })
+        saveNotes(notes)
+        console.log('New Note Added!')
+    } else {
+        console.log('Note title already excists!')
+    }
+
+}
+
+const saveNotes = (notes) => {
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
+}
+
+const loadNotes = () => {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    } catch (e) {
+        return []
+    }
+
+}
+
+module.exports = {
+    getNotes,
+    addNote
+}
 
